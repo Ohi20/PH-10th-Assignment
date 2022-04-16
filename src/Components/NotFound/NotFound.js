e7c3bd2f3615f0b8eeb89b86baf60936396aa255
Ohi97 <ashrafohi20@gmail.com>
2022-04-17 04:31:47 +0600

    404

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
         <div>
-            <h2>404</h2>
+            <h2 className='notfound-header'>Not Found!</h2>
+            <div className='notFound'>
+                <h2>Sorry the page you are looking for is not found!</h2>
+                <h1>Try another Route.</h1>
+            </div>
         </div>
     );
 };
